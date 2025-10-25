e42c9cb5b98723d6803e7380dfe0f5e532a6193b
Piotr <hunter976@gmail.com>
2025-10-25 22:16:20 +0200

    finished design

---
+++ src/comps/InputComponent.tsx
@@ -0,0 +1,8 @@
+export default function InputComponent() {
+  return (
+    <div className="grid gap-1">
+      <p className="uppercase">cardholder name</p>
+      <input type="text" className="w-full rounded-md border-2 p-2" />
+    </div>
+  );
+}
